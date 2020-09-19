c4b7198b96ec000fbf1d3e8e5b50311f55fd9d55
mlkylatte <sasazzara@gmail.com>
2020-09-19 14:15:20 -0700

    first commit

---
+++ Program Biodata_Azzuraa S.A_19081010033.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\tBIODATA PRIBADI\n\n");
+	printf("Nama : Azzuraa Shabika Aqmarina\n");
+	printf("NPM : 19081010033\n");
+	printf("Jenis Kelamin : Perempuan\n");
+	printf("Tempat Lahir : Surabaya\n");
+	printf("Tanggal Lahir : 15 September 2000\n");
+	printf("Alamat : Pulo Wonokromo 297 Surabaya\n");
+	printf("Email : sasazzara@gmail.com\n");
+	printf("Agama : Islam\n");
+	printf("Kewarganegaraan : Indonesia\n");
+	printf("Status : Mahasiswa\n");
+	printf("Motto Hidup : Learn from the past, live for the today, and plan for tomorrow\n");
+	return 0;	
+}
